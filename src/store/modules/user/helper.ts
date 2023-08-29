19e99b290c70b4f32ff5cb656ccf9abda58aac44
JW CHEN <39663688+cjw-666@users.noreply.github.com>
2023-08-29 10:38:01 +0800

    Update helper.ts

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -15,9 +15,9 @@ export interface UserState {
 export function defaultSetting(): UserState {
   return {
     userInfo: {
-      avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
-      name: 'ChenZhaoYu',
-      description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >GitHub</a>',
+      avatar: 'https://www.xstech.top/wp-content/uploads/2023/03/128x128.png',
+      name: 'GPT 4.0',
+      description: '  <a href="#" class="text-blue-500" target="_blank" > </a>',
     },
   }
 }
